604172da53a4b6fffaba4971a179723e700ed4c7
Peter Kwamena Essibu <164653045+peterkessibu@users.noreply.github.com>
2025-02-09 13:51:16 +0000

    Refactor layout and header components: replace font with Poppins, simplify Header component structure

--- components/Header.tsx
+++ components/Header.tsx
@@ -1,13 +1,10 @@
-import React from "react";
-
-const Header: React.FC = () => {
+export default function Header() {
   return (
     <header className="flex justify-center items-center border p-4 h-16 bg-gray-100">
-      <div className="text-xl font-bold">Chat + 
-        <span className="text-blue-500 ml-2">{" "} chatbot</span>
+      <div className="text-xl font-bold">
+        Chat +<span className="text-blue-500 ml-2"> chatbot</span>
       </div>
     </header>
   );
 };
 
-export default Header;
